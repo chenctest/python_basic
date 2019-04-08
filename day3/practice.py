a18acf06a67400caef1c1aaaba80ecee8f69893b
cc <412189127@qq.com>
2019-04-08 21:59:14 +0800

    提交

--- day3/practice.py
+++ day3/practice.py
@@ -1,2 +1,5 @@
 # Author :zhouyun
-print('gittest')
+msg = '\nhello,{name},how are you\n!'
+promp = input('name:')
+if promp.isdigit() != True:
+    print(msg.format_map({'name':promp}))
---
+++ day3/答案_menu.py
@@ -0,0 +1,63 @@
+# Author :zhouyun
+menu = {
+    '北京':{
+        '海淀':{
+            '五道口':{
+                'soho':{},
+                '网易':{},
+                'google':{}
+            },
+            '中关村':{
+                '爱奇艺':{},
+                '汽车之家':{},
+                'youku':{},
+            },
+            '上地':{
+                '百度':{},
+            },
+        },
+        '昌平':{
+            '沙河':{
+                '老男孩':{},
+                '北航':{},
+            },
+            '天通苑':{},
+            '回龙观':{},
+        },
+        '朝阳':{},
+        '东城':{},
+    },
+    '上海':{
+        '闵行':{
+            "人民广场":{
+                '炸鸡店':{}
+            }
+        },
+        '闸北':{
+            '火车战':{
+                '携程':{}
+            }
+        },
+        '浦东':{},
+    },
+    '山东':{},
+}
+
+
+exit_flag = False
+current_layer = menu
+
+layers = [menu]
+
+while not  exit_flag:
+    for k in current_layer:
+        print(k)
+    choice = input(">>:").strip()
+    if choice == "b":
+        current_layer = layers[-1]
+        #print("change to laster", current_layer)
+        layers.pop()
+    elif choice not in current_layer:continue
+    else:
+        layers.append(current_layer)
+        current_layer = current_layer[choice]
