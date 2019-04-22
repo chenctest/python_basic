b755ee0b947c40bfe4108435ac3a1b48492ef4d9
cc <412189127@qq.com>
2019-04-22 21:43:45 +0800

    2019422

--- AutoTest/autotest/autotest/urls.py
+++ AutoTest/autotest/autotest/urls.py
@@ -20,4 +20,6 @@ from apitest import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', views.login),
+    path('home/', views.home),
+    path('logout/', views.logout),
 ]
